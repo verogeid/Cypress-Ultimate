1478469ea21ec63461a1c65fedb4011ee44aaaf4
Diego González Fernández <162438292+verogeid@users.noreply.github.com>
2025-01-16 01:41:20 +0100

    Actualizar fixturesHandler.ts

--- scripts/fixturesHandler.ts
+++ scripts/fixturesHandler.ts
@@ -7,37 +7,33 @@ export const addFixtureReferences = (
   baseDir: string,
   log: string[]
 ) => {
-  log.push('Entering addFixtureReferences function');
+  console.log('Iniciando addFixtureReferences');
+  
+  const fileContent = fs.readFileSync(filePath, 'utf-8');
+  const fixtureRegex = /cy\.fixture['"]([^'"]+)['"]/g;
+  let match;
+  
+  while ((match = fixtureRegex.exec(fileContent)) !== null) {
+    let fixturePath = match[1];
 
-  try {
-    const fileContent = fs.readFileSync(filePath, 'utf-8');
-    const fixtureRegex = /cy\.fixture['"]([^'"]+)['"]/g;
-    let match;
-
-    while ((match = fixtureRegex.exec(fileContent)) !== null) {
-      let fixturePath = match[1];
-
-      log.push(`Found fixture path: ${fixturePath}`);
-
-      // Rutas de fixtures con cypress/fixtures y extensión .json
-      if (!fixturePath.startsWith('cypress/fixtures/')) {
-        fixturePath = 'cypress/fixtures/' + fixturePath;
-        log.push(`Added 'cypress/fixtures/' to path: ${fixturePath}`);
-      }
-
-      fixturePath = fixturePath + '.json';
-
-      if (!allFiles.has(fixturePath)) {
-        references.add(fixturePath);
-        allFiles.add(fixturePath);
-        log.push(`Added fixture reference: ${fixturePath}`);
-      }
+    console.log(`Encontrado fixture: ${fixturePath}`);
+    
+    // Rutas de fixtures con cypress/fixtures y extensión .json
+    if (!fixturePath.startsWith('cypress/fixtures/')) {
+      fixturePath = 'cypress/fixtures/' + fixturePath;
+      console.log(`Ruta de fixture modificada: ${fixturePath}`);
     }
-  } catch (error: any) {
-    log.push(`Error in addFixtureReferences: ${error.message}`);
-    throw error;
+    fixturePath = fixturePath + '.json';
+
+    if (!allFiles.has(fixturePath)) {
+      references.add(fixturePath);
+      allFiles.add(fixturePath);
+      console.log(`Fixture agregado: ${fixturePath}`);
+    } else {
+      console.log(`Fixture ya procesado: ${fixturePath}`);
+    }
   }
 
-  log.push('Exiting addFixtureReferences function');
+  console.log('addFixtureReferences completado');
 };
 
